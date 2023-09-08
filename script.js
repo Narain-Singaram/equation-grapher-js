be205f9165a9ed447c8fc1815219087b4fe7af33
Narain Singaram <narainsingaram7@gmail.com>
2023-09-07 21:03:46 -0400

    it works!

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById("graph");
 const ctx = canvas.getContext("2d");
 
-// Set canvas size to match the window size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -15,17 +14,14 @@ graphButton.addEventListener("click", () => {
     const equationText = equationInput.value;
     
     try {
-        // Replace ^ with ** and x with x*x
-        const cleanedEquationText = equationText.replace(/\^/g, '**').replace(/x/g, 'x*x');
-        
-        const parsedEquation = math.compile(cleanedEquationText);
-        
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         ctx.strokeStyle = "blue";
         
+        const expr = math.compile(equationText);
+        
         for (let x = -canvas.width / 2; x < canvas.width / 2; x += 1 / scaleX) {
-            const y = parsedEquation.evaluate({ x }) * scaleY;
+            const y = expr.evaluate({ x }) * scaleY;
             ctx.lineTo(canvas.width / 2 + x * scaleX, canvas.height / 2 - y);
         }
         
